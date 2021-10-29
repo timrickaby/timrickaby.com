bbc8360371f95a823fb0d256a62fe1fd8d7932f4
hello@timrickaby.com <hello@timrickaby.com>
2021-10-29 12:21:27 +0100

    Initial commit.

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+    header("HTTP/1.0 404 Not Found");
+    echo '<html>
+            <head>
+                <meta http-equiv="Refresh" content="0;url=https://timrickaby.com" />
+            </head>
+			<body></body>
+          </html>';
+?>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Tim Rickaby – Designer. Coder. Occasional Writer.</title>	
+
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+		<meta name="robots" content="noarchive"> 
+		<meta name="googlebot" content="noarchive">
+		
+		<link rel="start" href="https://timrickaby.com">
+
+		<link rel="icon" type="image/png" href="/assets/favicon.ico">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+		<meta name="p:domain_verify" content="ecd67e7cfd7da9d69351db64e3107e74" />
+		<meta name="pinterest" content="nopin" />
+		
+		<link rel="preconnect" href="https://fonts.googleapis.com"> 
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+		<link href="https://fonts.googleapis.com/css2?family=Spectral:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+
+		<link rel="stylesheet" id="style-css" href="/style.css" type="text/css" media="all">
+
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-NPLW4HTPTF"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-NPLW4HTPTF');
+		</script>
+	</head>
+	
+	<body>
+		<div id="page" class="row">
+			<div id="identity" class="row">
+				<img src="/assets/tim-rickaby-profile.jpg">
+			</div>
+
+			<div id="profile" class="row">
+				<h1>Tim Rickaby</h1>
+				<h2>Designer. Coder. Occasional Writer.</h2>
+
+				<p>
+					Hi, I'm Tim, a multi-disciplinary designer and developer crafting brand identities, print layouts, digital interfaces, websites and copy.
+					I've worked agency side with global brands like Adidas, Reebok, Virgin and Sky
+					plus freelance with many wonderful small businesses, startups, charities, artists and musicians.
+					<br /><br />
+
+					Right now I'm the design manager at yarn and craft brand
+					<a href="https://instagram.com/westyorkshirespinners" target="_blank" title="instagram.com/westyorkshirespinners">West Yorkshire Spinners</a> 
+					and I create bespoke work for private clients through my design and development studio, <a href="https://modocodo.studio" target="_blank" alt="modocodo.studio">Modocodo</a>.
+					<br /><br />
+
+					I've recently written and released <a href="https://awesomeexporter.com" target="_blank" title="awesomeexporter.com">Awesome Exporter</a>, an app for Adobe InDesign to simplify content creation
+					for studios and publishers when producing artwork, asset libraries and multi-lingual files. Other projects and experiments can be found on <a href="https://github.com/timrickaby" target="_blank" title="github.com/timrickaby">Github</a>.
+					<br /><br />
+					
+					Away from work I'll be walking around Yorkshire, taking photos and writing. <?php /* - occasionally publishing some under alter ego, Bimble Kid. */ ?>
+				</p>
+
+				<p>
+					<b>Contact</b>
+					<br /><br />
+					
+					Available for remote freelance work and contract hire. To discuss a project please get in touch with the studio: <a href="mailto:hello@modocodo.studio" target="_blank" title="hello@modocodo.studio">hello@modocodo.studio</a>
+					<br /><br />
+
+					For personal messages: <a href="mailto:hello@timrickaby.com" target="_blank" title="hello@timrickaby.com">hello@timrickaby.com</a>
+					<br /><br />
+					
+					<a href="https://www.linkedin.com/in/timrickaby" target="_blank" title="linkedin.com/modocodo">Work history available on LinkedIn</a>
+					<br /><br />
+					
+					<a href="https://instagram.com/timrickaby" target="_blank" title="instagram.com/timrickaby">Follow on Instagram</a>
+				</p>
+			</div>
+		
+			<div id="footer" class="row">
+				© 2021
+			</div>
+		</div>
+	</body>
+</html>
