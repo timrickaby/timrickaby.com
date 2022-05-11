00f5266c31843f8d8a5c1327d0537c14370e8e60
Tim Rickaby <hello@timrickaby.com>
2022-05-11 18:51:29 +0100

    Profile updates.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Tim Rickaby - Designer & Coder</title>	
+		<title>Tim Rickaby - Designer & Developer</title>	
 
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		
@@ -19,7 +19,8 @@
 		<link rel="preconnect" href="https://fonts.googleapis.com"> 
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
 		<link href="https://fonts.googleapis.com/css2?family=Spectral:wght@200;300;400;500;600&display=swap" rel="stylesheet">
-
+		
+		<link rel="stylesheet" href="https://use.typekit.net/dfk0buz.css">
 		<link rel="stylesheet" id="style-css" href="/style.css" type="text/css" media="all">
 
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-NPLW4HTPTF"></script>
@@ -39,33 +40,33 @@
 
 			<div id="profile" class="row">
 				<h1>Tim Rickaby</h1>
-				<h2>Designer & Coder</h2>
+				<h2>Designer & Developer</h2>
 
-				<p>Hi, I'm Tim. A designer and coder crafting brand identities, digital design, websites and short-form copy
-				to deliver unique, meaningful and effective creative and technical solutions.</p>
+				<p>Crafting brand identities, digital design, websites and short-form copy to deliver creative, technical and strategic solutions which are unique, meaningful and effective.</p>
 					
-				<p>I've worked agency side for brands like Adidas, Reebok, Virgin and Sky, plus in-house and freelance with many wonderful SME's, family businesses, startups, charities, artists and musicians.
+				<p>
+				As a designer I've worked agency-side with global brands and in-house with many wonderful SME’s, family businesses, charities and musicians. 
 				Right now I'm the design manager at leading wool and craft brand <a href="https://instagram.com/westyorkshirespinners" target="_blank">West Yorkshire Spinners</a> 
 				and I create bespoke work for private clients through my small studio, <a href="https://modocodo.studio" target="_blank">Modocodo</a>.</p>
 				
-				<p>I have also developed a couple of software products, one is <?php /*<a href="https://awesomeexporter.com" target="_blank">*/ ?>Awesome Exporter<?php /*</a>*/ ?>
-				– an Adobe InDesign extension enabling studios and publishers to simplify the delivery of artwork, assets and multi-lingual editorial files.
-				Other projects and experiments are available on <a href="https://github.com/timrickaby" target="_blank">Github</a>.</p>
-					
-				<p><?php /* Away from work I'll likely be walking, photographing, writing and occasionally publishing the results under alter ego, Bimble Kid.*/ ?></p>
+				<p>I’m also a self-taught developer with a love for technology, building projects in JavaScript, PHP and VB and recently releasing 
+				<?php <a href="https://awesomeexporter.com" target="_blank"> ?>Awesome Exporter</a> – 
+				an Adobe InDesign extension written in JavaScript which enables studios and publishers to simplify the delivery of artwork, assets and multi-lingual editorial files. 
+				Other projects and experiments are available on <a href="https://github.com/timrickaby" target="_blank">GitHub</a>.
 
 				<h3>Contact</h3>
-				<p>To discuss a project or idea, drop the studio an email: <a href="mailto:hello@modocodo.studio" target="_blank">hello@modocodo.studio</a></p>
+				<p>To discuss a project or idea, drop an email to: <a href="mailto:hello@modocodo.studio" target="_blank">hello@modocodo.studio</a></p>
 				<p>For personal messages: <a href="mailto:hello@timrickaby.com" target="_blank">hello@timrickaby.com</a></p>
 
 				<h3>Links</h3>
 				<p><a href="https://modocodo.studio" target="_blank">Modocodo Studio</a></p>
 				<p><a href="https://www.linkedin.com/in/timrickaby" target="_blank">LinkedIn</a></p>
+				<p><a href="https://github.com/timrickaby" target="_blank">GitHub</a></p>
 				<p><a href="https://www.instagram.com/timrickaby/" target="_blank">Instagram</a></p>
 			</div>
 		
 			<div id="footer" class="row">
-				© 2021
+				© 2022
 			</div>
 		</div>
 	</body>
